a34ae9d3997e1e637b97de17e9b0d869ccf11f3d
Jonathan Kelley <jkelleyrtp@gmail.com>
2018-11-01 23:57:20 -0400

    Added test kernels

--- kernels/test-kernel.c
+++ kernels/test-kernel.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(){
-  for(int offset = -12; offset< 27; offset++){
-    printf(offset);
-  }
+  for(int offset = -12; offset< 15; offset++){
+    printf("%i", offset+12);
+  };
+
+
 }
